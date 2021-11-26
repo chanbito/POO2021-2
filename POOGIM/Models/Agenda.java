cbe9b4d3634d4a217e3f48645f233fe63db144f8
gabriel marafiga <gabrielmarafiga@hotmail.com>
2021-11-26 00:00:56 -0300

    Diagrama

--- POOGIM/Models/Agenda.java
+++ POOGIM/Models/Agenda.java
@@ -1,28 +1,28 @@
 import java.util.Hashtable;
 public class Agenda {
-    private Hashtable<DiasDaSemana,Treino> agendaTreino;
-    private Hashtable<DiasDaSemana,Alimentacao> agendaDieta;
+    private Hashtable<DiaDaSemana,Treino> agendaTreino;
+    private Hashtable<DiaDaSemana,Alimentacao> agendaDieta;
 
-    public void setAgendaTreino(DiasDaSemana dia, Treino T) {
+    public void setAgendaTreino(DiaDaSemana dia, Treino T) {
             if(agendaTreino == null){
-                agendaTreino = new Hashtable<DiasDaSemana,Treino>();
+                agendaTreino = new Hashtable<DiaDaSemana,Treino>();
             }
             agendaTreino.put(dia, T);
     }
 
-    public void setAgendaDieta(DiasDaSemana dia, Alimentacao dieta) {
+    public void setAgendaDieta(DiaDaSemana dia, Alimentacao dieta) {
         if(agendaDieta == null){
-            agendaDieta = new Hashtable<DiasDaSemana, Alimentacao>();
+            agendaDieta = new Hashtable<DiaDaSemana, Alimentacao>();
         }
         agendaDieta.put(dia, dieta);
 
     }
 
-    public Hashtable<DiasDaSemana, Treino> getAgendaTreino() {
+    public Hashtable<DiaDaSemana, Treino> getAgendaTreino() {
         return agendaTreino;
     }
 
-    public Hashtable<DiasDaSemana, Alimentacao> getAgendaDieta() {
+    public Hashtable<DiaDaSemana, Alimentacao> getAgendaDieta() {
         return agendaDieta;
     }
 
@@ -31,6 +31,6 @@ public class Agenda {
         return "Agenda [agendaDieta = " + agendaDieta + ", agendaTreino = " + agendaTreino + "]";
     }
 }
-enum DiasDaSemana{
+enum DiaDaSemana{
     segunda, terca, quarta, quinta, sexta, sabado, domingo;
 }
--- POOGIM/view/Academia.java
+++ POOGIM/view/Academia.java
@@ -4,11 +4,11 @@ public class Academia {
         var nutri = new colaborador("maria", "maria@teste", 103, 3.50, "Nutricionista");
         var prof = new colaborador("Joao", "joao@teste", 102, 5.50, "Personal Treiner");
         var aluno = new Aluno("Gabriel", "Teste@Teste.com.br", 10000010);
-        aluno.agenda.setAgendaTreino(DiasDaSemana.segunda, new Treino(prof.get_id(), "Perna", "Agachamento", "3/20"));
-        aluno.agenda.setAgendaTreino(DiasDaSemana.quinta, new Treino(prof.get_id(), "Perna", "Agachamento", "3/20"));
+        aluno.agenda.setAgendaTreino(DiaDaSemana.segunda, new Treino(prof.get_id(), "Perna", "Agachamento", "3/20"));
+        aluno.agenda.setAgendaTreino(DiaDaSemana.quinta, new Treino(prof.get_id(), "Perna", "Agachamento", "3/20"));
         var dieta = new Alimentacao(nutri.get_id(), "Pre-Treino", "Dieta rica em batata", 3000);
-        aluno.agenda.setAgendaDieta(DiasDaSemana.domingo, dieta);
-        aluno.agenda.setAgendaDieta(DiasDaSemana.quarta, dieta);
+        aluno.agenda.setAgendaDieta(DiaDaSemana.domingo, dieta);
+        aluno.agenda.setAgendaDieta(DiaDaSemana.quarta, dieta);
         System.out.println(aluno);
         System.out.println(aluno.agenda);
     }
